a058252c7644f0035afb028a1a8f355fcc9fc361
LuisGandora <pnoyboy122@gmail.com>
2026-03-22 23:22:55 -0400

    Added specifications for the CLI side program

--- main.cpp
+++ main.cpp
@@ -27,10 +27,47 @@ int main(int argc, char* argv[]) {
 
     if (endsWith(path, ".csv")) {
         cout << "CSV detected" << endl;
+        ofstream output = ofstream("public/uploads/encoded.txt" , ios_base::trunc);
+        ifstream input = ifstream(path);
+        //loop through path and compress all of the data (store row by row sep by '>')
+
+        //input in huffman tree for creation
+
+        //get preorder for huffman tree creation
+
+        //get encoded string
+
+        /*
+        push huffman tree to the txt files
+            -Need a length var in bin to designate the size of the tree to decode
+            -(sort by pair values (char:bin string) where a parent node that doesnt have a char
+         is given unique char ('>') (Make sure to notify users that this char is not allowed))
+            -NEEDS a function for bin conversion
+
+        push the encoded string message into the txt files
+            -DOES NOT NEED function for bin conversion, just uses built in huffman tree
+
+        */
     }
 
     else if (endsWith(path, ".txt")) {
         cout << "TXT detected" << endl;
+        //Get the two paths for writing and reading
+
+        /*
+            Read the key for header based on structure and decode into a string seperated by spaces:
+                -Length of how many 'nodes' are in tree
+                -Node data (convert to string)
+                    -char is 1 byte so read one bin char
+                        -normal chars: fine to pair as "char: binstring"
+                        -nodes with no chars (parent nodes): pair as "'>'": binstring
+        */
+
+        //recreate huffman tree based on this pattern
+        
+        //Then push the second encoded csv into the huffman tree and obtain the decoded message
+
+        //Then reconstruct a csv file (like txt with new lines) and then end this portion
     }
 
     else {
